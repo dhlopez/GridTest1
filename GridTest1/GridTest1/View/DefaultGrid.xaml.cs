00ae30897550b1feeab09b1ef1b3e7ebdd999f49
David Lopez <david@2100computerlane.net>
2016-10-22 09:16:05 -0400

    Add project files.

---
+++ GridTest1/GridTest1/View/DefaultGrid.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace GridTest1.View
+{
+    public partial class DefaultGrid : ContentPage, INotifyPropertyChanged
+    {
+        int i = 0;
+        public DefaultGrid()
+        {
+            InitializeComponent();
+            var imgTapEvent = new TapGestureRecognizer();
+            imgTapEvent.Tapped += (sender, e) => methodImg1();
+            img1.GestureRecognizers.Add(imgTapEvent);
+        }
+
+        void methodImg1()
+        {
+            i++;
+            this.d10.Text = i.ToString();
+            //await DisplayAlert("Title", i+"", "OK");
+        }
+    }
+}
---
+++ GridTest1/View/DefaultGrid.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace GridTest1.View
+{
+    public partial class DefaultGrid : ContentPage
+    {
+        public DefaultGrid()
+        {
+            InitializeComponent();
+            var imgTapEvent = new TapGestureRecognizer();
+            imgTapEvent.Tapped += (sender, e) => methodImg1();
+            img1.GestureRecognizers.Add(imgTapEvent);
+        }
+
+        async void methodImg1()
+        {
+            await DisplayAlert("Title","Method methodImg1 was clicked","OK");
+        }
+    }
+}
