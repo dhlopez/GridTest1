22dfe59866659b905cf86e1010317fc767fed8ad
David Lopez <dhelaman@hotmail.com>
2016-10-26 08:00:18 -0400

    timer issue

--- GridTest1/GridTest1/View/DefaultGrid.xaml.cs
+++ GridTest1/GridTest1/View/DefaultGrid.xaml.cs
@@ -25,20 +25,22 @@ namespace GridTest1.View
             var imgTapEvent4 = new TapGestureRecognizer();
             var imgTapEvent5 = new TapGestureRecognizer();
             var imgTapEvent6 = new TapGestureRecognizer();
+            var startEvent = new TapGestureRecognizer();
             //List<Action> imgMethods = new List<Action>() { methodImg(List<Image>)};
-            
+
 
             /*foreach (Image img in images)
             {
                 imgTapEvent.Tapped += (sender, e) => methodImg(object sender, EventArgs args);
                 img.GestureRecognizers.Add(imgTapEvent);
             }*/
-            imgTapEvent1.Tapped += (sender, e) => methodImg(((Image)sender));
-            imgTapEvent2.Tapped += (sender, e) => methodImg(((Image)sender));
-            imgTapEvent3.Tapped += (sender, e) => methodImg(((Image)sender));
-            imgTapEvent4.Tapped += (sender, e) => methodImg(((Image)sender));
-            imgTapEvent5.Tapped += (sender, e) => methodImg(((Image)sender));
-            imgTapEvent6.Tapped += (sender, e) => methodImg(((Image)sender));
+            imgTapEvent1.Tapped += (sender, e) => methodImg(sender);
+            imgTapEvent2.Tapped += (sender, e) => methodImg(sender);
+            imgTapEvent3.Tapped += (sender, e) => methodImg(sender);
+            imgTapEvent4.Tapped += (sender, e) => methodImg(sender);
+            imgTapEvent5.Tapped += (sender, e) => methodImg(sender);
+            imgTapEvent6.Tapped += (sender, e) => methodImg(sender);
+            startEvent.Tapped += (sender, e) => methodImg(sender);
 
             img1.GestureRecognizers.Add(imgTapEvent1);
             img2.GestureRecognizers.Add(imgTapEvent2);
@@ -46,13 +48,36 @@ namespace GridTest1.View
             img4.GestureRecognizers.Add(imgTapEvent4);
             img5.GestureRecognizers.Add(imgTapEvent5);
             img6.GestureRecognizers.Add(imgTapEvent6);
-            
+            start.GestureRecognizers.Add(startEvent);
+
+            foreach (Image img in images)
+            {
+                /*
+                if (img.StyleId != null && img.Opacity == 0)
+                {
+                    img.Opacity = 100;
+                }
+                */
+                if (img.StyleId != null && img.Opacity == 1)
+                {
+                    img.Opacity = 0;
+                }
+            }
             //StartTimer(int);
+            
         }
-        void methodImg(Image img)
+
+        void methodImg(object sender)
         {
+            //Pattern(3);
+            img2.Opacity = 1;
+            StartTimer(6);
+            img4.Opacity = 1;
+
             //string x = sender.StyleId;
             //if(img.StyleId.Equals("img1"))
+
+            /*
             int imgName = Convert.ToInt16(img.Opacity);
             if (imgName > 0)
             {
@@ -62,30 +87,8 @@ namespace GridTest1.View
             {
                 img.Opacity = 1;
             }
-           
-            /*
-            foreach(Image img in images)
-            {
-                if (img.StyleId != null && img.Opacity == 0)
-                {
-                    img.Opacity = 100;
-                }
-                if (img.StyleId != null && img.Opacity == 100)
-                {
-                    img.Opacity = 0;
-                }
-            }
             */
         }
-        /*
-        void methodImg6()
-        {
-            if (img6.Opacity == 0)
-                img6.Opacity = 100;
-            else
-                img6.Opacity = 0;
-        }
-        */
         public void StartTimer(int interval)
         {
             int number = 0;
@@ -95,6 +98,7 @@ namespace GridTest1.View
                 {
                     //this.time.Text = number.ToString();
                     this.time.Text = TimeSpan.FromSeconds(number).ToString();
+                    img3.Opacity = 1;
                     return true; 
                 }
                 return false;
@@ -102,24 +106,47 @@ namespace GridTest1.View
             });
         }
 
-        public void Pattern()
+        public static List<string> pattern = new List<string>();
+
+        void Pattern(int initial)
         {
-            int initial = 3;
             List<string> options = new List<string>() { "img1", "img2", "img3", "img4", "img5", "img6" };
-            List<string> pattern = new List<string>();
+            //List<string> pattern = new List<string>();
             //populate pattern list
-            int i = 0;
-            while (true)
+
+            //while (true)
+            if (pattern.Count == 0)
             {
-                Random rnd = new Random();
-                int optionNumber = rnd.Next(0, 5);
-                pattern.Add(options[optionNumber]);
-                i++;
-                if (i >= 3)
+                for (int l = 0; l < initial; l++)
                 {
-                    //start game
+                    pattern.Add(options[GetNumber()]);
                 }
             }
+            else
+            {
+                pattern.Add(options[GetNumber()]);
+            }
+            
+            foreach (string opt in pattern)
+            {
+                foreach(Image img in images)
+                if (img.StyleId.Equals(opt))
+                {
+                    img.Opacity = 1;
+                    StartTimer(5);
+                    //img.Opacity = 0;
+                }
+            }
+            if (i >= 3)
+            {
+                //start game
+            }
+        }
+        public int GetNumber()
+        {
+            Random rnd = new Random();
+            int optionNumber = rnd.Next(0, 5);
+            return optionNumber;
         }
     }
 }
